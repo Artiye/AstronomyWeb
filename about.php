a4309e2f86797acc921c63144e9871f79d6346ec
Art <artinjobro@gmail.com>
2023-02-26 22:53:43 +0100

    fix

--- about.php
+++ about.php
@@ -75,45 +75,4 @@ $astronomy= new astronomy();
     }
     ?>
 </body>
-</html><!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="nav.css">
-        <link rel="stylesheet" href="about.css">
-        <script src="nav.js" defer></script>
-    <title>&#128301; About</title>
-</head>
-<body>
-    <nav>
-        <a href="about.html"><h1 class="logo">SPACED</h1></a>
-        <a href="#" class="nav-button">
-           <span class="bar"></span>
-           <span class="bar"></span>
-           <span class="bar"></span>
-        </a>
-        <div class="links">
-        <ul>
-            <li><a href="home.html">Home</a></li>
-            <li><a href="news.html">News</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="contact.html">Contact</a></li>
-            <li><a href="login.html">Login</a></li>
-        </ul>
-        </div>
-     </nav>
-      <section>
-        <main>
-               <h1 class="animate heading">Welcome to <b>SPACED</b></h1>
-               <p class="animate paragraph1">From the beginning of time, we have been watching the stars, and today we continue to make exciting discoveries.</p>
-               <p class="animate paragraph2">This website is created to guide your exploration of the universe and help you expand your knowledge</p>
-               <p class="animate paragraph3"> spaced@studio.com</p>
-     </main>
-      </section>
-</body>
-</html> -->
+</html>
