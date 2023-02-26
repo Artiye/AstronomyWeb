0335895ee1cf36fe1a7c5cf03c46fa650d526d0a
Art <artinjobro@gmail.com>
2023-02-26 21:50:39 +0100

    styling changes

--- contactdashboard.php
+++ contactdashboard.php
@@ -25,104 +25,20 @@ if($_SESSION['UserType'] !== "admin"){
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="dashboard.css">
     <title>dashboard</title>
-    <style>
-     
-      *{
-     font-family: 'Manrope', sans-serif;
-       }
-
-     body{
-        margin:3rem;
-        padding:0;
-        background-color: rgb(86, 87, 92);
-        color:white;
-        overflow-x: hidden;
-        display:flex;
-        justify-content: center;
-        align-items: center;
-     }
-
-     .dashboard{
-        width:100%;
-        height:fit-content;
-        padding-bottom:3rem;
-        display:flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content:center;
-
-     }
-     
-     .sbuttons{
-        width:100%;
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin: 0 1rem 3rem;
-     }
-
-     .button{
-        border-style: none;
-        background-color: rgb(255, 254, 254);
-        padding:9px;
-        opacity:.6;
-        transition: .5s;
-     }
-
-     .button:hover{
-        opacity:.8;
-        transition: .5s;
-     }
-
-     .button>a{
-        text-decoration: none;
-        color:rgb(0, 0, 0);
-     }
-     
-     th,td{
-        border-bottom: 1px solid white;
-        border-right:1px solid white;
-     }
-     table {
-        table-layout: fixed;
-       width: 100%;
-     }
-
-     .actions{
-        display: flex;
-        justify-content: flex-end;
-        gap:1rem;
-        padding-right:5px;
-     }
-     
-     .actions a{
-        color: white; 
-     }
-
-     .delete{
-        background-color: rgb(183, 6, 6);
-        
-     }
-     
-
-     .edit{
-        background-color: rgb(49, 50, 49);
-     }
-     
-     
-    </style>
+    
 </head>
 <body>
    
     <div class="dashboard">
 
-     <h1>SPACED | Dashboard</h1>
+    <h1>SPACED <span style="border-left:2px solid white;padding-left:6px;">Contact Dashboard</span></h1>
 
 
      <div class="buttons">
-        <button class="button "><a href="shtoUser.php">Add New User</a></button>
         <button class="button "><a href="home1.php">Home</a></button>
+        <button class="button "><a href="dashboard.php">Dashboard</a></button>
      </div>
      <table>
     
